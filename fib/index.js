3be4036176e290fbc1a4e2edccf8df06e15683b7
Sammy <ksamy90@aol.com>
2021-09-01 15:37:35 +0300

    pyramid-fib data

--- fib/index.js
+++ fib/index.js
@@ -19,21 +19,24 @@ function memoize(fn) {
     return result;
   };
 }
-// function slowFib(n) {
-//   if (n < 2) {
-//     return n;
-//   }
-//   return fib(n - 1) + fib(n - 2);
-// }
-// const fib = memoize(slowFib);
 
-function fib(n) {
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
 
-//[0,1]
+const fib = memoize(slowFib);
+
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//   return result[n];
+// }
 
 module.exports = fib;
